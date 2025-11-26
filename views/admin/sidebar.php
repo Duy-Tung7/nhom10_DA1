2d9c83f7887dffbd1342bde5d755efbda4330b9b
Duy-Tung7 <tunghdph40513@fpt.edu.vn>
2025-11-26 17:03:47 +0700

    .

--- views/admin/sidebar.php
+++ views/admin/sidebar.php
@@ -2,9 +2,9 @@
 $categoryModel = new Category();
 $categories = $categoryModel->getList();
 ?>
-
+<a href="<?= BASE_URL ?>?action=login">Đăng xuất</a>
 <div class="list-group">
-    <a href="<?= BASE_URL ?>?action=admin-dashboard" class="list-group-item list-group-item-action">Dashboard</a>
+    <a href="<?= BASE_URL ?>?action=admin-sidebar" class="list-group-item list-group-item-action">Dashboard</a>
 
     <a class="list-group-item list-group-item-action" data-bs-toggle="collapse" href="#categorySubmenu" role="button" aria-expanded="false" aria-controls="categorySubmenu">
        Quản lý tour
@@ -19,4 +19,4 @@ $categories = $categoryModel->getList();
     </div>
     
     <a href="<?= BASE_URL ?>?action=admin-list-tour" class="list-group-item list-group-item-action">Quản lý tour</a>
-</div>
+</div>  
